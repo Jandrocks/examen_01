f79a049ce9a673a32354770d7c41ece95992ffff
Jandrocks <32751139+Jandrocks@users.noreply.github.com>
2022-09-25 18:24:02 -0300

    Add files via upload

---
+++ assets/js/sweetalert.js
@@ -0,0 +1,64 @@
+// uso de libreria de alerta
+
+export function alerta() {
+  Swal.fire({
+    title: "Lo Sentimos!",
+    text: "no encontramos tu busqueda",
+    icon: "warning",
+    //html:
+    // icon:
+    // confirmButtonText:
+    // footer:
+    // width:
+    // padding:
+    // background:
+    // grow:
+    // backdrop:
+    // timer:
+    // timerProgressBar:
+    // toast:
+    // position:
+    // allowOutsideClick:
+    // allowEscapeKey:
+    allowEnterKey: "true",
+    // stopKeydownPropagation:
+
+    // input:
+    // inputPlaceholder:
+    // inputValue:
+    // inputOptions:
+
+    //  customClass:
+    // 	container:
+    // 	popup:
+    // 	header:
+    // 	title:
+    // 	closeButton:
+    // 	icon:
+    // 	image:
+    // 	content:
+    // 	input:
+    // 	actions:
+    // 	confirmButton:
+    // 	cancelButton:
+    // 	footer:
+
+    // showConfirmButton:
+    // confirmButtonColor:
+    // confirmButtonAriaLabel:
+
+    // showCancelButton:
+    // cancelButtonText:
+    // cancelButtonColor:
+    // cancelButtonAriaLabel:
+
+    // buttonsStyling:
+    // showCloseButton:
+    // closeButtonAriaLabel:
+
+    // imageUrl:
+    // imageWidth:
+    // imageHeight:
+    // imageAlt:
+  });
+}
