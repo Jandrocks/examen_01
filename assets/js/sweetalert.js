60f24ae2b18846329ef4c052a4e0fd2960b3cf0f
Jandrocks <32751139+Jandrocks@users.noreply.github.com>
2022-09-25 18:26:41 -0300

    Update sweetalert.js

--- assets/js/sweetalert.js
+++ assets/js/sweetalert.js
@@ -5,60 +5,6 @@ export function alerta() {
     title: "Lo Sentimos!",
     text: "no encontramos tu busqueda",
     icon: "warning",
-    //html:
-    // icon:
-    // confirmButtonText:
-    // footer:
-    // width:
-    // padding:
-    // background:
-    // grow:
-    // backdrop:
-    // timer:
-    // timerProgressBar:
-    // toast:
-    // position:
-    // allowOutsideClick:
-    // allowEscapeKey:
     allowEnterKey: "true",
-    // stopKeydownPropagation:
-
-    // input:
-    // inputPlaceholder:
-    // inputValue:
-    // inputOptions:
-
-    //  customClass:
-    // 	container:
-    // 	popup:
-    // 	header:
-    // 	title:
-    // 	closeButton:
-    // 	icon:
-    // 	image:
-    // 	content:
-    // 	input:
-    // 	actions:
-    // 	confirmButton:
-    // 	cancelButton:
-    // 	footer:
-
-    // showConfirmButton:
-    // confirmButtonColor:
-    // confirmButtonAriaLabel:
-
-    // showCancelButton:
-    // cancelButtonText:
-    // cancelButtonColor:
-    // cancelButtonAriaLabel:
-
-    // buttonsStyling:
-    // showCloseButton:
-    // closeButtonAriaLabel:
-
-    // imageUrl:
-    // imageWidth:
-    // imageHeight:
-    // imageAlt:
   });
 }
